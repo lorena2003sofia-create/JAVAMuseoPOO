3ec2d5aa9e7966a3786be0212c992cec3036bbd9
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-04-05 18:01:17 -0500

    Creacion clase abstracta ObraArte

---
+++ src/ObraArte.java
@@ -0,0 +1,30 @@
+abstract class ObraArte {
+
+    protected String autor;
+    protected String periodo;
+    protected double valor;
+    protected String fechaCreacion;
+    protected String fechaIngreso;
+
+    public ObraArte(String autor,String periodo,double valor,
+    String fechaCreacion,String fechaIngreso){
+
+        this.autor=autor;
+        this.periodo=periodo;
+        this.valor=valor;
+        this.fechaCreacion=fechaCreacion;
+        this.fechaIngreso=fechaIngreso;
+
+    }
+
+    public void mostrarInformacion(){
+
+        System.out.println("Autor: "+autor);
+        System.out.println("Periodo: "+periodo);
+        System.out.println("Valor: "+valor);
+
+    }
+
+    public abstract void mostrarDetalle();
+
+}
