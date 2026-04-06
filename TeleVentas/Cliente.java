e3fb050cf1f3c5b22ee21b2151ecb6794202018e
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-04-06 00:08:05 -0500

    Create Cliente.java

---
+++ TeleVentas/Cliente.java
@@ -0,0 +1,20 @@
+class Cliente extends Persona{
+
+    private String direccion;
+
+    public Cliente(String nombre,String email,
+    String telefono,String direccion){
+
+        super(nombre,email,telefono);
+
+        this.direccion=direccion;
+
+    }
+
+    public void comprar(){
+
+        System.out.println("Cliente realizando compra");
+
+    }
+
+}
